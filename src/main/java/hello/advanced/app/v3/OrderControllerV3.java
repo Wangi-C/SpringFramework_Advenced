3d2515fe286ee33f5f90128337fac1adb17713e5
WangiC <ChoWangi>
2022-03-29 18:27:20 +0900

    세션2_로그남기기(동시성 문제)     **Application을 구동시키고 OrderController에 두개의 요청이 1초안에 들어왔을 때, traceId와 level 값에 문제가 발생한다. 이것은 Configuration에 FieldLogTrace가 싱글톤 방식으로 빈 등록 되어있는데 FieldLogTrace필드에 TraceId가 인스턴스 변수로 지정되어 있기 때문이다.

---
+++ src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            //로그는 기존 애플리케이션 동작에 영향을 주면 안됌 -> 로그에서 예외를 먹어버리기 때문에 다시 예외를 던져줘야됌
+            throw e;
+        }
+    }
+}
